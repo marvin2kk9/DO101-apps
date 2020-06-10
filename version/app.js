9ba56542e2737316379cfea0236f5885fb764c35
arevalo <arevalo@mx1.ibm.com>
2020-06-10 17:09:55 -0500

    v1 of the app

--- version/app.js
+++ version/app.js
@@ -10,7 +10,8 @@ app.get('/', function (req, res) {
     //send the response to the client
     res.send(response);
 
-});
+}); 
+
 
 app.listen(8080, function () {
   console.log('Server listening on port 8080...');
